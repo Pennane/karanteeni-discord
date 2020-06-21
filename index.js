5a80acacc95111970fa870c6472618e4b1fff591
Arttu Pennanen <arttu@pennanen.org>
2020-06-21 18:31:25 +0300

    Refactoring & stability

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 process.chdir(__dirname)
 
+let startingDate = Date.now()
+
 const schedule = require('node-schedule')
 const Discord = require('discord.js')
 const chalk = require('chalk')
@@ -13,76 +15,6 @@ const client = new Discord.Client()
 let cachedServerStatus = null;
 let cachedMemberCount = null;
 
-function fetchServerStatus() {
-    return new Promise((resolve, reject) => {
-        axios.get('https://api.mcsrvstat.us/2/mc.karanteeni.net')
-            .then(function (response) {
-                resolve(response.data)
-            })
-            .catch(function (error) {
-                reject(error)
-            });
-    });
-}
-
-
-async function updateServerStatus() {
-    let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
-    let onServer = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.CURRENT_PLAYERS_ON_SERVER);
-    let onDiscord = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.ALL_PLAYERS_ON_DISCORD);
-
-    let serverStatus = await fetchServerStatus();
-    if (!cachedServerStatus || serverStatus.players.online !== cachedServerStatus.players.online) {
-        onServer.setName("Pelaajia servulla: " + serverStatus.players.online);
-    }
-
-    if (!cachedMemberCount || guild.memberCount !== cachedMemberCount) {
-        onDiscord.setName("Pelaajia discordissa: " + guild.memberCount);
-    }
-
-    cachedServerStatus = serverStatus;
-}
-
-async function updateGivenRoles() {
-    let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
-    let channel = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.CHANNEL_RULES)
-    let message = channel.messages.cache.get(configuration.ID_MAP.MESSAGES.RULE_REACTION_MESSAGE)
-    let playerRoles = await message.reactions.cache.get("✅").users.fetch()
-    let notifyRoles = await message.reactions.cache.get("❗").users.fetch()
-
-    playerRoles.each(player => {
-        let member = guild.members.cache.get(player.id)
-        if (!member) return;
-        let roleName = "Pelaaja"
-        let role = guild.roles.cache.find(role => role.name === roleName)
-        if (member.roles.cache.find(role => role.name === roleName)) return;
-        member.roles.add(role);
-    })
-
-    notifyRoles.each(player => {
-        let member = guild.members.cache.get(player.id)
-        if (!member) return;
-        let roleName = "Ilmoitukset"
-        let role = guild.roles.cache.find(role => role.name === roleName)
-        if (member.roles.cache.find(role => role.name === roleName)) return;
-        member.roles.add(role);
-    })
-}
-
-function toggleRole(user, roleName, type) {
-    let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
-    if (!guild) throw new Error("no guild bro")
-    let role = guild.roles.cache.find(role => role.name === roleName)
-
-    let member = guild.members.cache.get(user.id)
-    if (type === "ADD") {
-        member.roles.add(role);
-    } else if (type === "REMOVE") {
-        member.roles.remove(role);
-    }
-
-}
-
 function parseReaction(reaction) {
     if (reaction.reaction.message.id === configuration.ID_MAP.MESSAGES.RULE_REACTION_MESSAGE) {
         if (reaction.reaction._emoji.name === "✅" && reaction.type === "ADD") {
@@ -92,6 +24,24 @@ function parseReaction(reaction) {
             toggleRole(reaction.user, "Ilmoitukset", reaction.type)
         }
     }
+}
+
+function toggleRole(user, roleName, type) {
+    let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
+    if (!guild) throw new Error("Client has an invalid main guild id")
+
+    let role = guild.roles.cache.find(role => role.name === roleName)
+    if (!role) throw new Error("Main guild does not have a role named '" + roleName + "'")
+
+    let member = guild.members.cache.get(user.id)
+    if (!member) throw new Error("Guild does not have received user as a member")
+
+    if (type === "ADD") {
+        member.roles.add(role);
+    } else if (type === "REMOVE") {
+        member.roles.remove(role);
+    }
+
 }
 
 function cacheRequiredMessages() {
@@ -109,22 +59,105 @@ function cacheRequiredMessages() {
 
 }
 
+function fetchServerStatus() {
+    return new Promise((resolve, reject) => {
+        axios.get('https://api.mcsrvstat.us/2/mc.karanteeni.net')
+            .then(function (response) {
+                resolve(response.data)
+            })
+            .catch(function (error) {
+                reject(error)
+            });
+    });
+}
+
+function updateServerStatus() {
+    return new Promise(async (resolve, reject) => {
+        let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
+        let onServer = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.CURRENT_PLAYERS_ON_SERVER);
+        let onDiscord = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.ALL_PLAYERS_ON_DISCORD);
+
+        let serverStatus = await fetchServerStatus();
+        if (!cachedServerStatus || serverStatus.players.online !== cachedServerStatus.players.online) {
+            onServer.setName("Pelaajia servulla: " + serverStatus.players.online);
+        }
+
+        if (!cachedMemberCount || guild.memberCount !== cachedMemberCount) {
+            onDiscord.setName("Pelaajia discordissa: " + guild.memberCount);
+        }
+
+        cachedServerStatus = serverStatus;
+        cachedMemberCount = guild.memberCount;
+
+        resolve(true)
+    })
+}
+
+function updateAutomatedRoles() {
+    return new Promise(async (resolve, reject) => {
+        let guild = client.guilds.cache.get(configuration.ID_MAP.GUILD)
+        if (!guild) throw new Error("Client has an invalid main guild id")
+
+        let channel = guild.channels.cache.get(configuration.ID_MAP.CHANNELS.CHANNEL_RULES)
+        if (!channel) throw new Error("Main guild does not have given channel as a part of it")
+
+        let message = channel.messages.cache.get(configuration.ID_MAP.MESSAGES.RULE_REACTION_MESSAGE)
+        if (!message) throw new Error("Did not find the role reaction message")
+
+
+        let playerRoles = await message.reactions.cache.get("✅").users.fetch()
+        let notifyRoles = await message.reactions.cache.get("❗").users.fetch()
+
+        playerRoles.each(player => {
+            let member = guild.members.cache.get(player.id)
+            if (!member) return;
+            let roleName = "Pelaaja"
+            let role = guild.roles.cache.find(role => role.name === roleName)
+            if (member.roles.cache.find(role => role.name === roleName)) return;
+            member.roles.add(role);
+        })
+
+        notifyRoles.each(player => {
+            let member = guild.members.cache.get(player.id)
+            if (!member) return;
+            let roleName = "Ilmoitukset"
+            let role = guild.roles.cache.find(role => role.name === roleName)
+            if (member.roles.cache.find(role => role.name === roleName)) return;
+            member.roles.add(role);
+        })
+
+        resolve(true)
+    })
+}
+
+
+
 client.on('ready', async () => {
+
+    // Cache messages required for updating roles
     await cacheRequiredMessages()
-    updateServerStatus()
-    updateGivenRoles()
+
+    // Update server status and add missing roles
+    await updateServerStatus()
+    await updateAutomatedRoles()
 
 
-    let serverStatusScheduler = schedule.scheduleJob('*/1 * * * *', () => {
+    // Update the info channel names in discord every 10 minutes
+    let serverStatusScheduler = schedule.scheduleJob('*/10 * * * *', () => {
         updateServerStatus()
     });
 
+    // Check that the bot has given necessary roles every hour
     let rolesUptodateScheduler = schedule.scheduleJob('* */1 * * *', () => {
-        updateGivenRoles()
+        updateAutomatedRoles()
     });
 
     console.log(chalk.blue("//// Botti virallisesti hereillä."))
+    console.log(chalk.blue("//// Käynnistyminen kesti"), chalk.red(Date.now() - startingDate), chalk.blue('ms'))
+    console.log(chalk.blue("//// Discord serverillä yhteensä", chalk.yellow(cachedMemberCount), chalk.blue('pelaajaa')))
+    console.log(chalk.blue("//// Minecraftissa tällä hetkellä", chalk.yellow(cachedServerStatus.players.online), chalk.blue('pelaajaa')))
 })
+
 
 client.on("messageReactionAdd", (reaction, user) => {
     parseReaction({
