4cd07f1675f5a394d0ebc888da301bb66102009b
Arttu Pennanen <arttu@pennanen.org>
2021-01-25 00:24:59 +0200

    Commands work in counting_game channel

--- commands/command_files/puhista.js
+++ commands/command_files/puhista.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
 const syntaxEmbed = require('../syntaxEmbed.js')
 
-let embed = new Discord.MessageEmbed().setColor(0xF4E542);
+let embed = new Discord.MessageEmbed().setColor(0xf4e542)
 
 const configuration = {
-    name: "puhista",
+    name: 'puhista',
     admin: true,
-    syntax: "puhista <määrä 1-99>",
-    desc: "poistaa kanavalta x määrän viestejä",
-    triggers: ["puhista"],
-    type: ["työkalut"],
+    syntax: 'puhista <määrä 1-99>',
+    desc: 'poistaa kanavalta x määrän viestejä',
+    triggers: ['puhista'],
+    type: ['työkalut'],
     requireGuild: false
 }
 
@@ -19,18 +19,18 @@ module.exports = {
         return new Promise(async (resolve, reject) => {
             if (args[1] >= 2 && args[1] <= 99) {
                 embed.setDescription(`Poistin ${args[1]} viestiä.`)
-                let amount = parseInt(args[1]) + 1;
+                let amount = parseInt(args[1]) + 1
                 await message.channel.bulkDelete(amount)
-                message.channel.send(embed)
-                    .then(message => message.delete({ timeout: 4000 }))
-                    .catch(err => console.info(err))
-
+                message.channel
+                    .send(embed)
+                    .then((message) => message.delete({ timeout: 4000 }))
+                    .catch((err) => console.info(err))
             } else {
                 let embed = syntaxEmbed({ configuration, args })
-                message.channel.send(embed).catch(err => console.info(err))
+                message.channel.send(embed).catch((err) => console.info(err))
             }
             resolve()
-        });
+        })
     },
     configuration
 }
--- message_handling/handler.js
+++ message_handling/handler.js
@@ -6,20 +6,11 @@ const { EventEmitter } = require('events')
 
 let prefix = configuration.PREFIX
 
-let countingChannelId = configuration.DISCORD.ID_MAP.CHANNELS.COUNT_UP_GAME
-
 let specialMessages = new EventEmitter()
 
 module.exports = {
     specialMessages,
     parse: function (message, client) {
-        if (message.channel.id === countingChannelId) {
-            return specialMessages.emit('countingGameMessage', {
-                message,
-                client
-            })
-        }
-
         let hasPrefix = message.content.startsWith(prefix)
 
         if (!hasPrefix && message.content.includes('bad bot')) {
@@ -37,11 +28,9 @@ module.exports = {
         let args = message.content.trim().substr(prefix.length).split(' ')
 
         let trigger = args[0].toLowerCase()
-
         if (!triggers.hasOwnProperty(trigger)) return
 
         let command = commands.get(triggers[trigger])
-
         command.execute(message, client, args)
     }
 }
